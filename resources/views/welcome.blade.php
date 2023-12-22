061cf5ef7d819523b09175f898e10d3e1cf06a5d
FLAVIO AMORUSO <flavioamoruso6.com>
2023-12-22 14:32:16 +0100

    aggiusti front-end homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,4 +43,50 @@
         
     </div>
 </div>
+
+
+<div class="container-fluid">
+  <div class="row justify-content-center align-items-center">
+    <div class="col-12 my-4 text-center">
+      <h5>Lorem ipsum.</h5>
+    </div>
+    <div class="col-12 col-md-6 d-flex justify-content-center mt-3">
+      <div class="card">
+        <img class="img-fluid" src="/img/sfondo2.jpg" alt="card">
+        <div class="card__content">
+          <p class="card__title">Card Title</p>
+          <p class="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        </div>
+      </div>
+      
+    </div>
+    <div class="col-12 col-md-6 mt-3 text-center">
+      <h5>Lorem, ipsum dolor.</h5>
+      <p class="text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, totam Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas atque earum dolorem molestias neque vitae sit labore debitis facere aspernatur in voluptatem, ipsa et officiis consequatur facilis laudantium odio recusandae.</p>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid mt-4">
+  <div class="row justify-content-center align-items-center  text-center">
+    <div class="col-12 col-md-6 mt-3">
+      <h5>Lorem, ipsum dolor.</h5>
+      <p class="text-white">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, totam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, iure aliquid. Exercitationem vitae, eveniet sit facilis sed consectetur corporis? In praesentium, fuga nesciunt sit cum animi deleniti voluptatibus rem itaque?</p>
+    </div>
+    <div class="col-12 col-md-6 d-flex justify-content-center mt-3">
+      <div class="card">
+        <img class="img-fluid" src="/img/sfondo.jpg" alt="card">
+        <div class="card__content">
+          <p class="card__title">Card Title</p>
+          <p class="card__description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+        </div>
+      </div>
+      
+    </div>
+
+  </div>
+</div>
+
+
+
 </x-layout>
